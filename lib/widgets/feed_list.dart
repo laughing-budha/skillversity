52bb8d8591f1676f7ba2050f7fdb71334d5fb620
Jose K James <josekjames5@gmail.com>
2023-10-28 22:35:46 +0530

    content

--- lib/widgets/feed_list.dart
+++ lib/widgets/feed_list.dart
@@ -4,12 +4,14 @@ final List imagenames = [
 ];
 
 final List titlenames = [
-  "The US Refuses to Fall in Love With Electric Cars",
-  "The Best Android Phones",
+  "Unleashing the Copyright Protection Jedi: Evaluating the Effectiveness of Dynamic Injunctions",
+  "Navigating Indian Copyright Framework in the Age of AI generated Works",
+  "Intermediary Liability in Copyright claim over User-Generated Content"
 ];
 final List feednames = [
-  "As China and Europe lead the race to make electric vehicles mainstream, America lags behind. This is a problem.",
-  "You'll see lots of ads encouraging you to upgrade to a 5G plan and buy a 5G phone. Yes, you do need a new phone that supports 5G"
+  "Exploring the innovative concept of dynamic injunctions in copyright protection, assessing their effectiveness in adapting to the rapidly evolving digital landscape, and how they empower rights holders to combat online infringement in real-time.",
+  "It delves into the legal intricacies and challenges surrounding copyright in India concerning AI-generated content, shedding light on the complexities of ownership, licensing, and protection in this emerging technological landscape",
+  "investigating the legal implications of intermediary liability in the context of user-generated content and copyright claims, exploring the responsibilities and potential defenses of online platforms when faced with allegations of copyright infringement by their users."
 ];
 
 final List authornames = [
