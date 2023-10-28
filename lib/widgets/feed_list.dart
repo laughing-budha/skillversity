895f8cd7e24f291d7d3d6578b9976126e4441ecd
Tomin-Joy <tominjk007@gmail.com>
2023-10-28 22:50:02 +0530

    [images]

--- lib/widgets/feed_list.dart
+++ lib/widgets/feed_list.dart
@@ -1,17 +1,18 @@
 final List imagenames = [
-  "assets/images/ai.jpeg",
+  "assets/images/ai.jpg",
   "assets/images/copy.png",
+  "assets/images/jedi.jfif",
 ];
 
 final List titlenames = [
   "Unleashing the Copyright Protection Jedi: Evaluating the Effectiveness of Dynamic Injunctions",
   "Navigating Indian Copyright Framework in the Age of AI generated Works",
-  "Intermediary Liability in Copyright claim over User-Generated Content"
+  "Intermediary Liability in Copyright claim over User-Generated Content",
 ];
 final List feednames = [
   "Assessing their effectiveness in adapting to the rapidly evolving digital landscape, and how they empower rights holders to combat online infringement in real-time.",
   "It delves into the legal intricacies and challenges surrounding copyright in India concerning AI-generated content, shedding light on the complexities of ownership, licensing, and protection in this emerging technological landscape",
-  "Exploring the responsibilities and potential defenses of online platforms when faced with allegations of copyright infringement by their users."
+  "Exploring the responsibilities and potential defenses of online platforms when faced with allegations of copyright infringement by their users.",
 ];
 
 final List authornames = [
